7327385eaba99feae2909ed9d270eae3f59ce16d
takara-kairimei <mayanovita0sari@gmail.com>
2026-06-03 20:23:02 +0700

    Fix db connection for Vercel

--- config/db.js
+++ config/db.js
@@ -3,13 +3,11 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000,
       family: 4
     });
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.error('MongoDB Connection Error:', err.message);
   }
 };
 
