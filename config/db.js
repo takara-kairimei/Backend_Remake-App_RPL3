c33d23280592e1b2c71d5a6f0a4c76faf2eca5c3
takara-kairimei <mayanovita0sari@gmail.com>
2026-06-03 21:22:41 +0700

    fix db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,34 @@
 const mongoose = require('mongoose');
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false, 
       family: 4
+    };
+    
+    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+      console.log('MongoDB connected');
+      return mongoose;
     });
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('MongoDB Connection Error:', err.message);
   }
+  
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 };
 
 module.exports = connectDB;
